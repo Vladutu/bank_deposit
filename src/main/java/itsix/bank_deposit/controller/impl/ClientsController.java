6869a76c3343e96ef9f8a3d461638bcecfac3171
Vladutu <vladutu_georgian_4d@yahoo.com>
2016-08-28 16:29:40 +0300

    - fixed NullPointerException when trying to update product info when no products are available

--- src/main/java/itsix/bank_deposit/controller/impl/ClientsController.java
+++ src/main/java/itsix/bank_deposit/controller/impl/ClientsController.java
@@ -1,10 +1,5 @@
 package itsix.bank_deposit.controller.impl;
 
-import java.io.Serializable;
-import java.util.List;
-
-import javax.swing.JOptionPane;
-
 import itsix.bank_deposit.builder.IAccountBuilder;
 import itsix.bank_deposit.builder.IClientBuilder;
 import itsix.bank_deposit.controller.IClientsController;
@@ -18,251 +13,252 @@ import itsix.bank_deposit.repository.IClientRepository;
 import itsix.bank_deposit.repository.IProductRepository;
 import itsix.bank_deposit.validator.IClientValidator;
 import itsix.bank_deposit.validator.IValidationResult;
-import itsix.bank_deposit.views.IBankAccountView;
-import itsix.bank_deposit.views.ICapitalizationButtonState;
-import itsix.bank_deposit.views.IMainView;
-import itsix.bank_deposit.views.INewAccountView;
-import itsix.bank_deposit.views.INewClientView;
-import itsix.bank_deposit.views.INewDepositView;
+import itsix.bank_deposit.views.*;
+
+import javax.swing.*;
+import java.io.Serializable;
+import java.util.List;
 
 public class ClientsController implements IClientsController, Serializable {
 
-	private transient IMainView mainView;
+    private transient IMainView mainView;
 
-	private transient INewClientView newClientView;
+    private transient INewClientView newClientView;
 
-	private transient IBankAccountView bankAccountView;
+    private transient IBankAccountView bankAccountView;
 
-	private transient INewAccountView newAccountView;
+    private transient INewAccountView newAccountView;
 
-	private transient INewDepositView newDepositView;
+    private transient INewDepositView newDepositView;
 
-	private IClientRepository clientRepository;
+    private IClientRepository clientRepository;
 
-	private IProductRepository productsRepository;
+    private IProductRepository productsRepository;
 
-	private IClientBuilder clientBuilder;
+    private IClientBuilder clientBuilder;
 
-	private IAccountBuilder accountBuilder;
+    private IAccountBuilder accountBuilder;
 
-	private IClientValidator clientValidator;
+    private IClientValidator clientValidator;
 
-	private IClient selectedClient = null;
+    private IClient selectedClient = null;
 
-	private IAccount selectedAccount = null;
+    private IAccount selectedAccount = null;
 
-	private IProduct selectedProduct = null;
+    private IProduct selectedProduct = null;
 
-	private ICapitalizationButtonState capitalizationButtonState;
+    private ICapitalizationButtonState capitalizationButtonState;
 
-	public ClientsController(IClientRepository clientRepository, IProductRepository productsRepository,
-			IClientBuilder clientBuilder, IClientValidator clientValidator, IAccountBuilder accountBuilder) {
-		this.clientRepository = clientRepository;
-		this.productsRepository = productsRepository;
-		this.clientBuilder = clientBuilder;
-		this.clientValidator = clientValidator;
-		this.accountBuilder = accountBuilder;
-	}
+    public ClientsController(IClientRepository clientRepository, IProductRepository productsRepository,
+                             IClientBuilder clientBuilder, IClientValidator clientValidator, IAccountBuilder accountBuilder) {
+        this.clientRepository = clientRepository;
+        this.productsRepository = productsRepository;
+        this.clientBuilder = clientBuilder;
+        this.clientValidator = clientValidator;
+        this.accountBuilder = accountBuilder;
+    }
 
-	@Override
-	public void setMainView(IMainView mainView) {
-		this.mainView = mainView;
-		mainView.disableButtons();
-	}
+    @Override
+    public void setMainView(IMainView mainView) {
+        this.mainView = mainView;
+        mainView.disableButtons();
+    }
 
-	@Override
-	public void openNewClientView() {
-		newClientView.showWindow();
-	}
+    @Override
+    public void openNewClientView() {
+        newClientView.showWindow();
+    }
 
-	@Override
-	public void saveClient() {
-		String ssn = newClientView.getSsn();
-		String firstName = newClientView.getFirstName();
-		String lastName = newClientView.getLastName();
-		String address = newClientView.getAddress();
+    @Override
+    public void saveClient() {
+        String ssn = newClientView.getSsn();
+        String firstName = newClientView.getFirstName();
+        String lastName = newClientView.getLastName();
+        String address = newClientView.getAddress();
 
-		IValidationResult result = clientValidator.validate(ssn, firstName, lastName, address);
+        IValidationResult result = clientValidator.validate(ssn, firstName, lastName, address);
 
-		if (!result.isValid()) {
-			JOptionPane.showMessageDialog(null, result.getErrorDescription());
+        if (!result.isValid()) {
+            JOptionPane.showMessageDialog(null, result.getErrorDescription());
 
-			return;
-		}
+            return;
+        }
 
-		try {
-			clientRepository.findBySsn(ssn);
-			JOptionPane.showMessageDialog(null, "Client already exists!");
-			return;
-		} catch (EntityNotFoundException e) {
-			IClient client = clientBuilder.build(ssn, firstName, lastName, address);
+        try {
+            clientRepository.findBySsn(ssn);
+            JOptionPane.showMessageDialog(null, "Client already exists!");
+            return;
+        } catch (EntityNotFoundException e) {
+            IClient client = clientBuilder.build(ssn, firstName, lastName, address);
 
-			clientRepository.save(client);
-			newClientView.closeWindow();
-		}
+            clientRepository.save(client);
+            newClientView.closeWindow();
+        }
 
-	}
+    }
 
-	@Override
-	public void searchClient() {
-		String ssn = mainView.getSearchClientSsn();
+    @Override
+    public void searchClient() {
+        String ssn = mainView.getSearchClientSsn();
 
-		try {
-			selectedClient = clientRepository.findBySsn(ssn);
+        try {
+            selectedClient = clientRepository.findBySsn(ssn);
 
-			mainView.setClientFields(selectedClient.getSsn(), selectedClient.getFirstName(),
-					selectedClient.getLastName(), selectedClient.getAddress(), selectedClient.getAccounts());
-			mainView.enableButtons();
+            mainView.setClientFields(selectedClient.getSsn(), selectedClient.getFirstName(),
+                    selectedClient.getLastName(), selectedClient.getAddress(), selectedClient.getAccounts());
+            mainView.enableButtons();
 
-			if (!selectedClient.canCreateBankAccount()) {
-				mainView.disableCreateNewAccountButton();
-			}
+            if (!selectedClient.canCreateBankAccount()) {
+                mainView.disableCreateNewAccountButton();
+            }
 
-		} catch (EntityNotFoundException e) {
-			selectedClient = null;
-			mainView.clearClientFields();
-			mainView.disableButtons();
-			JOptionPane.showMessageDialog(null, "Client not found!");
-		}
+        } catch (EntityNotFoundException e) {
+            selectedClient = null;
+            mainView.clearClientFields();
+            mainView.disableButtons();
+            JOptionPane.showMessageDialog(null, "Client not found!");
+        }
 
-	}
+    }
 
-	@Override
-	public void setNewClientView(INewClientView newClientView) {
-		this.newClientView = newClientView;
-	}
+    @Override
+    public void setNewClientView(INewClientView newClientView) {
+        this.newClientView = newClientView;
+    }
 
-	@Override
-	public void updateClient() {
-		if (selectedClient == null) {
-			JOptionPane.showMessageDialog(null, "No client available!");
+    @Override
+    public void updateClient() {
+        if (selectedClient == null) {
+            JOptionPane.showMessageDialog(null, "No client available!");
 
-			return;
-		}
+            return;
+        }
 
-		String firstName = mainView.getClientFirstName();
-		String lastName = mainView.getClientLastName();
-		String address = mainView.getClientAddress();
+        String firstName = mainView.getClientFirstName();
+        String lastName = mainView.getClientLastName();
+        String address = mainView.getClientAddress();
 
-		IValidationResult result = clientValidator.validate(firstName, lastName, address);
+        IValidationResult result = clientValidator.validate(firstName, lastName, address);
 
-		if (!result.isValid()) {
-			JOptionPane.showMessageDialog(null, result.getErrorDescription());
+        if (!result.isValid()) {
+            JOptionPane.showMessageDialog(null, result.getErrorDescription());
 
-			return;
-		}
+            return;
+        }
 
-		selectedClient.update(firstName, lastName, address);
-		JOptionPane.showMessageDialog(null, "Client updated!");
-	}
+        selectedClient.update(firstName, lastName, address);
+        JOptionPane.showMessageDialog(null, "Client updated!");
+    }
 
-	@Override
-	public void openBankAccountView() {
-		selectedAccount = mainView.getSelectedBankAccount();
-		bankAccountView.show(selectedAccount);
-		selectedAccount.subscribe(bankAccountView);
+    @Override
+    public void openBankAccountView() {
+        selectedAccount = mainView.getSelectedBankAccount();
+        bankAccountView.show(selectedAccount);
+        selectedAccount.subscribe(bankAccountView);
 
-	}
+    }
 
-	@Override
-	public void setBankAccountView(IBankAccountView bankAccountView) {
-		this.bankAccountView = bankAccountView;
-	}
+    @Override
+    public void setBankAccountView(IBankAccountView bankAccountView) {
+        this.bankAccountView = bankAccountView;
+    }
 
-	@Override
-	public void depositMoney() {
-		int money = bankAccountView.getMoneyAmount();
-		selectedAccount.deposit(money);
+    @Override
+    public void depositMoney() {
+        int money = bankAccountView.getMoneyAmount();
+        selectedAccount.deposit(money);
 
-	}
+    }
 
-	@Override
-	public void withdrawMoney() {
-		int money = bankAccountView.getMoneyAmount();
+    @Override
+    public void withdrawMoney() {
+        int money = bankAccountView.getMoneyAmount();
 
-		try {
-			selectedAccount.withdraw(money);
-		} catch (InvalidOperationException e) {
-			JOptionPane.showMessageDialog(null, "Founds not enough to withdaw");
-		}
+        try {
+            selectedAccount.withdraw(money);
+        } catch (InvalidOperationException e) {
+            JOptionPane.showMessageDialog(null, "Founds not enough to withdaw");
+        }
 
-	}
+    }
 
-	@Override
-	public void setNewAccountView(INewAccountView newAccountView) {
-		this.newAccountView = newAccountView;
+    @Override
+    public void setNewAccountView(INewAccountView newAccountView) {
+        this.newAccountView = newAccountView;
 
-	}
+    }
 
-	@Override
-	public void openNewAccountView() {
-		List<ICurrency> currencies = selectedClient.getRemainingCurrenciesForAccounts();
-		newAccountView.show(currencies);
-	}
+    @Override
+    public void openNewAccountView() {
+        List<ICurrency> currencies = selectedClient.getRemainingCurrenciesForAccounts();
+        newAccountView.show(currencies);
+    }
 
-	@Override
-	public void createNewAccount() {
-		ICurrency currency = newAccountView.getSelectedCurrency();
-		int initialDeposit = newAccountView.getInitialDeposit();
+    @Override
+    public void createNewAccount() {
+        ICurrency currency = newAccountView.getSelectedCurrency();
+        int initialDeposit = newAccountView.getInitialDeposit();
 
-		IAccount account = accountBuilder.build(currency, initialDeposit);
-		selectedClient.addAccount(account);
+        IAccount account = accountBuilder.build(currency, initialDeposit);
+        selectedClient.addAccount(account);
 
-		if (!selectedClient.canCreateBankAccount()) {
-			mainView.disableCreateNewAccountButton();
-		}
+        if (!selectedClient.canCreateBankAccount()) {
+            mainView.disableCreateNewAccountButton();
+        }
 
-		mainView.updateAccountsTable();
-		newAccountView.closeWindow();
-	}
+        mainView.updateAccountsTable();
+        newAccountView.closeWindow();
+    }
 
-	@Override
-	public void setNewDepositView(INewDepositView newDepositView) {
-		this.newDepositView = newDepositView;
+    @Override
+    public void setNewDepositView(INewDepositView newDepositView) {
+        this.newDepositView = newDepositView;
 
-	}
+    }
 
-	@Override
-	public void openNewDepositView() {
-		List<IProduct> products = productsRepository.getProducts();
-		newDepositView.show(products);
-	}
+    @Override
+    public void openNewDepositView() {
+        List<IProduct> products = productsRepository.getProducts();
+        newDepositView.show(products);
+    }
 
-	@Override
-	public void updateProductInfo() {
-		IProduct product = newDepositView.getSelectedProduct();
+    @Override
+    public void updateProductInfo() {
+        IProduct product = newDepositView.getSelectedProduct();
 
-		newDepositView.updateProductInfo(product);
+        if (product != null) {
+            newDepositView.updateProductInfo(product);
+        }
 
-	}
+    }
 
-	@Override
-	public void changeRenewalState() {
-		capitalizationButtonState = capitalizationButtonState.nextState();
-		capitalizationButtonState.execute();
-	}
+    @Override
+    public void changeRenewalState() {
+        capitalizationButtonState = capitalizationButtonState.nextState();
+        capitalizationButtonState.execute();
+    }
 
-	@Override
-	public void changeCapitalizationState() {
-		// TODO Auto-generated method stub
+    @Override
+    public void changeCapitalizationState() {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public void setCapitalizationButtonState(ICapitalizationButtonState capitalizationButtonState) {
-		this.capitalizationButtonState = capitalizationButtonState;
-		capitalizationButtonState.execute();
+    @Override
+    public void setCapitalizationButtonState(ICapitalizationButtonState capitalizationButtonState) {
+        this.capitalizationButtonState = capitalizationButtonState;
+        capitalizationButtonState.execute();
 
-	}
+    }
 
-	@Override
-	public void onProductSelect() {
-		// if (selectedProduct != null) {
-		// selectedProduct.resetState();
-		// }
+    @Override
+    public void onProductSelect() {
+        // if (selectedProduct != null) {
+        // selectedProduct.resetState();
+        // }
 
-		selectedProduct = newDepositView.getSelectedProduct();
-		newDepositView.resetButtons();
-	}
+        selectedProduct = newDepositView.getSelectedProduct();
+        newDepositView.resetButtons();
+    }
 
 }
